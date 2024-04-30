6f42acdcb465cabe1074ffc7498df2ef57057574
shakiqurrahman <rahmanshakiqur@gmail.com>
2024-04-30 23:10:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const loginText = "<h1>Please Login First!!</h1>"
+const registarText = '<h3>Before Login, You must have to register!!</h3>'
+const userData = {
+    "shake" : "web developer",
+    "shama" : "queen"
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+});
+
+app.get("/login", (req, res) => {
+  res.send(loginText);
+});
+
+app.get('/registar', (req, res) => {
+    res.send(registarText)
+})
+
+app.get('/user', (req, res) => {
+    res.send(userData);
+})
+
+app.listen(port, () => {
+  console.log(`App is running at port: ${port}`);
+});
